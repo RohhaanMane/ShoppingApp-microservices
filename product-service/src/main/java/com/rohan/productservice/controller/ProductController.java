f73affaa121ff1b19f402d946e391720a44d011c
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-09 14:12:52 +0530

    Product service - Add new Product created

--- product-service/src/main/java/com/rohan/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/rohan/productservice/controller/ProductController.java
@@ -1,5 +1,26 @@
 package com.rohan.productservice.controller;
 
+import com.rohan.productservice.dto.ProductRequest;
+import com.rohan.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+
+    private final ProductService productService;
+
+//    public ProductController(ProductService productService){
+//        this.productService = productService;
+//    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+
+    }
 }
---
+++ product-service/src/main/java/com/rohan/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.rohan.productservice.service;
+
+import com.rohan.productservice.dto.ProductRequest;
+import com.rohan.productservice.model.Product;
+import com.rohan.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+
+    }
+}
