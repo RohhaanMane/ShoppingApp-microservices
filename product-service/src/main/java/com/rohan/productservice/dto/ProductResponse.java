7cb6d9bfc9af3b48b75b9c4016d06a4be18c85d2
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-09 14:28:38 +0530

    sample commit

--- product-service/src/main/java/com/rohan/productservice/dto/ProductResponse.java
+++ product-service/src/main/java/com/rohan/productservice/dto/ProductResponse.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductResponse {
+public class ProductResponse { 
     private String id;
     private String name;
     private String description;
